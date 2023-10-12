0dc2bffcba09687a3be848f58fe67cacf622a5fc
Zoyal1 <147695316+Zoyal1@users.noreply.github.com>
2023-10-12 21:33:53 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -4,7 +4,7 @@ const BoxOldWeb = () => {
 		<div id="BoxOldWeb">
 			<a href="https://www.tiktok.com/@synkratimenos.9">
 				<div className="flex justify-between">
-					<img src="/dist/Tiktok.svg" alt="" className="w-auto h-6" />
+					<img src="/dist/Tiktok.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
 				<h1 className="text-white text-base font-semibold pr-0 mt-5">Follow Tiktok Kita Juga Yaaa</h1>
